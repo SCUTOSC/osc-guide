2e3d95c385c5930df121ba81ca6f6b60870f5938
Samuka007 <samuka007@dragonos.org>
2024-09-26 07:05:48 +0000

    Deploying to gh-pages from @ SCUTOSC/osc-guide@86913bb67bd139f495e36734a72c8f67c60ddd6b 🚀

---
+++ assets/index.html-6iH-R1A-.js
@@ -0,0 +1,24 @@
+import{_ as e,c as t,a as o,o as a}from"./app-BEfrsClv.js";const i={};function n(s,r){return a(),t("div",null,[o(` # highlights:
+#   - header: 可定制的页面
+#     description: 完整无障碍支持的可定制外观
+#     image: /assets/image/ui.svg
+#     bgImage: https://theme-hope-assets.vuejs.press/bg/9-light.svg
+#     bgImageDark: https://theme-hope-assets.vuejs.press/bg/9-dark.svg
+#     highlights:
+#       - title: 深色模式
+#         icon: circle-half-stroke
+#         details: 可以自由切换浅色模式与深色模式
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/darkmode.html
+
+#       - title: 主题色切换
+#         icon: palette
+#         details: 支持自定义主题色并允许用户在预设的主题颜色之间切换
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/theme-color.html
+
+#       - title: 更多
+#         icon: ellipsis
+#         details: RTL 布局，打印支持，全局按钮等
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/others.html
+
+# copyright: Samuka007
+# footer: 使用 <a href="https://theme-hope.vuejs.press/zh/" target="_blank">VuePress Theme Hope</a> 主题 | CC-BY-SA-4.0 协议, 版权所有 © 2024-至今 `)])}const l=e(i,[["render",n],["__file","index.html.vue"]]),h=JSON.parse('{"path":"/","title":"Home","lang":"zh-CN","frontmatter":{"home":true,"icon":"mdi:home","title":"Home","heroImage":"/osc_home.png","heroText":"OSC Guide","heroFullScreen":false,"tagline":"由华工开源俱乐部，携手众学生，共同倾心打造的 CS 指南。从入门到进阶，不论应试、就业、科研，我们总以发展为导向。","actions":[{"text":"开始阅读","icon":"fa-solid:lightbulb","link":"./guide/","type":"primary"},{"text":"编写指南","icon":"fa-solid:edit","link":"./demo/"}],"highlights":[{"header":"DragonOS 龙操作系统","image":"https://bbs.dragonos.org.cn/uploads/default/original/1X/482f64f4f9678cf9de0504e4add3839dcaa75709.png","highlights":[{"title":"Linux兼容性","icon":"mdi:linux","details":"二进制兼容Linux。Linux软件能轻松迁移到DragonOS。<br />点击前往官网","link":"https://dragonos.org/"},{"title":"Rust内核全国第2名","icon":"mdi:language-rust","details":"DragonOS使用Rust语言从0开始研发内核及用户态环境。按GitHub Star排序，位居全国第2名。<br />点击前往仓库","link":"https://github.com/DragonOS-Community/DragonOS"},{"title":"社区驱动开发","icon":"fa-solid:users","details":"DragonOS是一个社区驱动的项目，将始终坚持开源，开放包容的精神。<br />点击前往论坛","link":"https://bbs.dragonos.org.cn/"}]}],"head":[["meta",{"property":"og:url","content":"https://guide.scutosc.cn/"}],["meta",{"property":"og:site_name","content":"OSC Guide"}],["meta",{"property":"og:title","content":"Home"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-09-26T05:51:43.000Z"}],["meta",{"property":"article:author","content":"SCUT Open Source Community"}],["meta",{"property":"article:modified_time","content":"2024-09-26T05:51:43.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"Home\\"}"]]},"headers":[],"git":{"createdTime":1726664908000,"updatedTime":1727329903000,"contributors":[{"name":"Samuka007","email":"samuka007@dragonos.org","commits":3},{"name":"Samuka007","email":"samuka007@qq.com","commits":2},{"name":"Samuel Dai","email":"samuka007@dragonos.org","commits":1}]},"readingTime":{"minutes":2.28,"words":685},"filePathRelative":"README.md","localizedDate":"2024年9月18日"}');export{l as comp,h as data};
