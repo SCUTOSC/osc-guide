5db6235ce9482a0437bf33d4ce17bf9694009e45
Samuka007 <samuka007@dragonos.org>
2024-10-04 04:43:29 +0000

    Deploying to gh-pages from @ SCUTOSC/osc-guide@d657338c0ea5e4c87ffc78445cb85d024b4834a8 🚀

---
+++ assets/index.html-D0TvP_J4.js
@@ -0,0 +1,24 @@
+import{_ as e,c as t,a as o,o as a}from"./app-BQKZ7r7r.js";const n={};function i(s,r){return a(),t("div",null,[o(` # highlights:
+#   - header: 可定制的页面
+#     description: 完整无障碍支持的可定制外观
+#     image: /assets/image/ui.svg
+#     bgImage: https://theme-hope-assets.vuejs.press/bg/9-light.svg
+#     bgImageDark: https://theme-hope-assets.vuejs.press/bg/9-dark.svg
+#     highlights:
+#       - title: 深色模式
+#         icon: circle-half-stroke
+#         details: 可以自由切换浅色模式与深色模式
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/darkmode.html
+
+#       - title: 主题色切换
+#         icon: palette
+#         details: 支持自定义主题色并允许用户在预设的主题颜色之间切换
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/theme-color.html
+
+#       - title: 更多
+#         icon: ellipsis
+#         details: RTL 布局，打印支持，全局按钮等
+#         link: https://theme-hope.vuejs.press/zh/guide/interface/others.html
+
+# copyright: Samuka007
+# footer: 使用 <a href="https://theme-hope.vuejs.press/zh/" target="_blank">VuePress Theme Hope</a> 主题 | CC-BY-SA-4.0 协议, 版权所有 © 2024-至今 `)])}const p=e(n,[["render",i],["__file","index.html.vue"]]),c=JSON.parse('{"path":"/","title":"Home","lang":"zh-CN","frontmatter":{"home":true,"icon":"mdi:home","title":"Home","heroImage":"/osc_home.png","heroText":"OSC Guide","heroFullScreen":true,"tagline":"由华工开源俱乐部，携手众学生，共同倾心打造的 CS 指南。从入门到进阶，不论应试、就业、科研，我们总以发展为导向。","actions":[{"text":"开始阅读","icon":"fa-solid:lightbulb","link":"./guide/","type":"primary"},{"text":"编写指南","icon":"fa-solid:edit","link":"./demo/"}],"head":[["meta",{"property":"og:url","content":"https://guide.scutosc.cn/"}],["meta",{"property":"og:site_name","content":"OSC Guide"}],["meta",{"property":"og:title","content":"Home"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-10-04T04:42:23.000Z"}],["meta",{"property":"article:author","content":"SCUT Open Source Community"}],["meta",{"property":"article:modified_time","content":"2024-10-04T04:42:23.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"Home\\"}"]]},"headers":[],"git":{"createdTime":1726664908000,"updatedTime":1728016943000,"contributors":[{"name":"Samuka007","email":"samuka007@dragonos.org","commits":4},{"name":"Samuka007","email":"samuka007@qq.com","commits":2},{"name":"Samuel Dai","email":"samuka007@dragonos.org","commits":1}]},"readingTime":{"minutes":2.28,"words":685},"filePathRelative":"README.md","localizedDate":"2024年9月18日"}');export{p as comp,c as data};
